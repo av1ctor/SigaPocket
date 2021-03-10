2eea02e5eed7d1f6bceb792fa9086f88967bff5f
av1ctor <av1ctortv@gmail.com>
2021-03-10 12:53:08 -0300

    adicionado wake lock para evitar que app seja finalizada quando em background

--- android/app/src/main/java/com/sigapocket/DocsSyncJob.java
+++ android/app/src/main/java/com/sigapocket/DocsSyncJob.java
@@ -10,6 +10,8 @@ import android.app.job.JobInfo;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.bridge.ReactContext;
@@ -21,23 +23,41 @@ import android.util.Log;
 public class DocsSyncJob extends JobService
 {
 	public static long INTERVAL = 60; // em segundos
-	
+	private WakeLock wakeLock = null;
+
 	@Override
 	public boolean onStartJob(JobParameters params) 
 	{
 		Context context = getApplicationContext();
 		
+		if(wakeLock == null)
+		{
+			PowerManager powerManager = (PowerManager)getSystemService(POWER_SERVICE);
+			wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "SigaPocket::DocsSyncJob");
+		}
+		
 		try
 		{
+			if(wakeLock != null)
+			{
+				wakeLock.acquire();	
+			}
 			sendEvent(getReactNativeHost().getReactInstanceManager().getCurrentReactContext(), "DocsSyncEvent", null);
 		}
 		catch(Exception e)
 		{
 			Log.e("SigaPocket", e.getMessage());
 		}
-			
-		Util.scheduleJob(context, INTERVAL, DocsSyncJob.class);
+		finally
+		{
+			Util.scheduleJob(context, INTERVAL, DocsSyncJob.class);
 
+			if(wakeLock != null && wakeLock.isHeld())
+			{
+				wakeLock.release();
+			}
+		}
+			
 		// não é necessário reagendar, porque foi feito manualmente acima
 		return false;
 	}
@@ -45,6 +65,11 @@ public class DocsSyncJob extends JobService
 	@Override
 	public boolean onStopJob(JobParameters params) 
 	{
+		if(wakeLock != null && wakeLock.isHeld())
+		{
+			wakeLock.release();
+		}
+
 		// se estiver sem conexão de rede, reiniciar job quando ela voltar
 		return true;
 	}
